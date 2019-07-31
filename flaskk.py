b23a14a42ac4662ae9bfc6be31422b5780da05e4
Nimit <nimitkhuranaa@gmail.com>
2019-07-31 17:10:05 +0530

    Updated a bit

--- flaskk.py
+++ flaskk.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
-# search for movie **given url #
+# search for movie **complete given url #
 @app.route("/")
 def data():
     return jsonp_converter("https://v2.sg.media-imdb.com/suggests/a/avengers.json")
@@ -24,6 +24,17 @@ def get_data():
 def create_strap():
     return render_template('strap.html')
 
+'''
+The term ‘web templating system’ refers to designing an HTML script in which the variable data can be inserted
+dynamically. A web template system comprises of a template engine, some kind of data source and a template processor.
+
+Flask uses jinga2 template engine. A web template contains HTML syntax interspersed placeholders for variables and
+expressions (in these case Python expressions) which are replaced values when the template is rendered.
+
+@app.route('/strap/<int:num>')                                {% for x in range(10) %}
+def create_strap_intvar(num):                                   <h> {{x}} </h>
+    return render_template('strap.html', x=num)               {%endfor%}
+'''
 
 if __name__ == "__main__":
     app.run(debug=True)
